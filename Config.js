3b3a303d0a04b6f1d780130a06cfb729b03758d3
CDNMD <chalahstatuschannel@gmail.com>
2025-02-03 21:32:39 +0530

    Update Config.js

--- Config.js
+++ Config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "VAJIRA-MD=mVV3HS7B#Bn7YqRC-JlvFvy9gPeTkWw3BXr0UB9aDkKtKnxgecWo",
+SESSION_ID: process.env.SESSION_ID || "Z3lXmRLR#DA3CH-X9RubOOMe3stLBdgHS6I7UkjrjJPpKc03njOo",
 MONGODB: process.env.MONGODB || "mongodb://mongo:YszgdBxTcgmCsigakakedaIIcsBhNGOU@junction.proxy.rlwy.net:11394",
 
-BOT_NUMBER: process.env.BOT_NUMBER || "94711262551"
+BOT_NUMBER: process.env.BOT_NUMBER || "94776938009"
 };
